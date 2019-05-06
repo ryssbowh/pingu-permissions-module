207af3ef8362cf3b06b60567c7ed4c343b2fbe2a
Unknown <blondin.boris@gmail.com>
2019-05-06 18:10:40 +0100

    first commit

---
+++ webpack.mix.js
@@ -0,0 +1,14 @@
+const mix = require('laravel-mix');
+const path = require('path');
+
+var dir = __dirname;
+var name = dir.split(path.sep).pop();
+
+var assetPath = __dirname + '/Resources/assets';
+var publicPath = path.resolve(__dirname,'../../public/modules/'+name);
+
+//Javascript
+mix.js(assetPath + '/js/app.js', publicPath + '/js/'+name+'.js').sourceMaps();
+
+//Css
+mix.sass(assetPath + '/sass/app.scss', publicPath + '/css/'+name+'.css');
