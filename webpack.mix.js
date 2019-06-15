d17033324118c40bfa02ec95f17d7b2b44cbf3cc
Unknown <blondin.boris@gmail.com>
2019-06-15 06:49:58 +0100

    modules assets

--- webpack.mix.js
+++ webpack.mix.js
@@ -5,7 +5,7 @@ var dir = __dirname;
 var name = dir.split(path.sep).pop();
 
 var assetPath = __dirname + '/Resources/assets';
-var publicPath = path.resolve(__dirname,'../../public/modules/'+name);
+var publicPath = path.resolve(__dirname,'../../public/module-assets/'+name);
 
 //Javascript
 mix.js(assetPath + '/js/app.js', publicPath + '/js/'+name+'.js').sourceMaps();
