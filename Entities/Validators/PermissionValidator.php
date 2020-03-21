9f2751ff32850a56af829f4dad35f5373e0ba19a
Boris Blondin <blondin.boris@gmail.com>
2020-03-21 18:35:05 +0000

    fixed fields

--- Entities/Validators/PermissionValidator.php
+++ Entities/Validators/PermissionValidator.php
@@ -6,7 +6,7 @@ use Pingu\Field\Support\FieldValidator\BaseFieldsValidator;
 
 class PermissionValidator extends BaseFieldsValidator
 {
-    protected function rules(): array
+    protected function rules(bool $updating): array
     {
         return [
 
